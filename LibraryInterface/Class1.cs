54d0e56d8cd5f6b96af803aa80f4c9ba42b6c19b
Fabianm2003 <152031664+Fabianm2003@users.noreply.github.com>
2024-05-22 19:09:27 +0200

    Add files via upload

---
+++ LibraryInterface/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryInterfaceWF
+{
+    internal class Class1
+    {
+    }
+}
