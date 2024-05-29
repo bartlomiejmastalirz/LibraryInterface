69fcb2113e3794d626485d78f34a0d1ef3cf3ea5
Fabian <F2003poczta@gmail.com>
2024-05-29 21:42:18 +0200

    Changes in a login and password textbox and add new icons.

--- WFInterface/Form1.Designer.cs
+++ WFInterface/Form1.Designer.cs
@@ -28,25 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             pictureBox1 = new PictureBox();
             label1 = new Label();
-            label2 = new Label();
             panel1 = new Panel();
-            label3 = new Label();
             panel2 = new Panel();
             loginButton = new Button();
             exitLabel = new Label();
             txtUsername = new TextBox();
             txtPassword = new TextBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.noun_library_71245;
-            pictureBox1.Location = new Point(112, 54);
+            pictureBox1.Location = new Point(128, 72);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(87, 71);
+            pictureBox1.Size = new Size(99, 95);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -55,49 +59,31 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Century Gothic", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Microsoft Sans Serif", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.Black;
-            label1.Location = new Point(91, 128);
-            label1.MinimumSize = new Size(110, 40);
+            label1.Location = new Point(104, 171);
+            label1.MinimumSize = new Size(126, 53);
             label1.Name = "label1";
-            label1.Size = new Size(127, 40);
+            label1.Size = new Size(160, 53);
             label1.TabIndex = 1;
             label1.Text = "LOG IN";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label2.Location = new Point(17, 194);
-            label2.Name = "label2";
-            label2.Size = new Size(87, 19);
-            label2.TabIndex = 2;
-            label2.Text = "Username";
             // 
             // panel1
             // 
             panel1.BackColor = Color.DimGray;
-            panel1.Location = new Point(17, 216);
+            panel1.Location = new Point(19, 288);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(279, 10);
+            panel1.Size = new Size(319, 13);
             panel1.TabIndex = 3;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label3.Location = new Point(17, 282);
-            label3.Name = "label3";
-            label3.Size = new Size(80, 19);
-            label3.TabIndex = 2;
-            label3.Text = "Password";
             // 
             // panel2
             // 
             panel2.BackColor = Color.DimGray;
-            panel2.Location = new Point(17, 304);
+            panel2.Location = new Point(19, 403);
+            panel2.Margin = new Padding(3, 4, 3, 4);
             panel2.Name = "panel2";
-            panel2.Size = new Size(279, 10);
+            panel2.Size = new Size(319, 15);
             panel2.TabIndex = 3;
             // 
             // loginButton
@@ -105,23 +91,24 @@
             loginButton.BackColor = Color.DimGray;
             loginButton.FlatAppearance.BorderSize = 0;
             loginButton.FlatStyle = FlatStyle.Flat;
-            loginButton.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            loginButton.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
             loginButton.ForeColor = Color.White;
-            loginButton.Location = new Point(22, 362);
+            loginButton.Location = new Point(25, 483);
+            loginButton.Margin = new Padding(3, 4, 3, 4);
             loginButton.Name = "loginButton";
-            loginButton.Size = new Size(265, 32);
+            loginButton.Size = new Size(303, 43);
             loginButton.TabIndex = 4;
             loginButton.Text = "Log in";
             loginButton.UseVisualStyleBackColor = false;
             loginButton.Click += loginButton_Click;
             // 
-            // label4
+            // exitLabel
             // 
             exitLabel.AutoSize = true;
-            exitLabel.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            exitLabel.Location = new Point(141, 416);
-            exitLabel.Name = "label4";
-            exitLabel.Size = new Size(29, 17);
+            exitLabel.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            exitLabel.Location = new Point(161, 555);
+            exitLabel.Name = "exitLabel";
+            exitLabel.Size = new Size(37, 20);
             exitLabel.TabIndex = 5;
             exitLabel.Text = "Exit";
             exitLabel.Click += exitLabel_Click;
@@ -129,47 +116,77 @@
             // txtUsername
             // 
             txtUsername.BorderStyle = BorderStyle.None;
-            txtUsername.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            txtUsername.ForeColor = Color.Black;
-            txtUsername.Location = new Point(105, 193);
+            txtUsername.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            txtUsername.ForeColor = Color.DarkGray;
+            txtUsername.Location = new Point(76, 243);
+            txtUsername.Margin = new Padding(3, 4, 3, 4);
             txtUsername.Multiline = true;
             txtUsername.Name = "txtUsername";
-            txtUsername.Size = new Size(182, 23);
+            txtUsername.Size = new Size(242, 38);
             txtUsername.TabIndex = 6;
+            txtUsername.Text = "Login";
+            txtUsername.Enter += txtUsername_Enter;
+            txtUsername.Leave += txtUsername_Leave;
             // 
             // txtPassword
             // 
             txtPassword.BorderStyle = BorderStyle.None;
-            txtPassword.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            txtPassword.ForeColor = Color.Black;
-            txtPassword.Location = new Point(105, 281);
+            txtPassword.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            txtPassword.ForeColor = Color.DarkGray;
+            txtPassword.Location = new Point(76, 355);
+            txtPassword.Margin = new Padding(3, 4, 3, 4);
             txtPassword.Multiline = true;
             txtPassword.Name = "txtPassword";
-            txtPassword.PasswordChar = '*';
-            txtPassword.Size = new Size(182, 23);
+            txtPassword.Size = new Size(242, 41);
             txtPassword.TabIndex = 6;
+            txtPassword.Text = "Password";
+            txtPassword.Enter += txtPassword_Enter;
+            txtPassword.Leave += txtPassword_Leave;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(25, 354);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(35, 42);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.icons8_user_35;
+            pictureBox3.Location = new Point(19, 242);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(41, 39);
+            pictureBox3.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox3.TabIndex = 8;
+            pictureBox3.TabStop = false;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(308, 486);
+            ClientSize = new Size(352, 648);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
             Controls.Add(txtPassword);
             Controls.Add(txtUsername);
             Controls.Add(exitLabel);
             Controls.Add(loginButton);
             Controls.Add(panel2);
             Controls.Add(panel1);
-            Controls.Add(label3);
-            Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -178,14 +195,14 @@
 
         private PictureBox pictureBox1;
         private Label label1;
-        private Label label2;
         private Panel panel1;
-        private Label label3;
         private Panel panel2;
         private Button loginButton;
         private Label exitLabel;
 
         private TextBox txtUsername;
         private TextBox txtPassword;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
     }
 }
--- WFInterface/Form1.cs
+++ WFInterface/Form1.cs
@@ -13,7 +13,7 @@ namespace WFInterface
         }
 
         private void loginButton_Click(object sender, EventArgs e)
-        {   
+        {
             //This piece of code checks for the username and password from database
             //TODO: Implement the csv OR database usage to this thing
             if (txtUsername.Text == "Test" && txtPassword.Text == "1234")
@@ -34,6 +34,42 @@ namespace WFInterface
         {
             //This just exits the application upon clicking "Exit"
             Application.Exit();
+        }
+
+        private void txtUsername_Enter(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Login")
+            {
+                txtUsername.Text = "";
+                txtUsername.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtUsername_Leave(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Login")
+            {
+                txtUsername.Text = "";
+                txtUsername.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "Password")
+            {
+                txtPassword.Text = "";
+                txtPassword.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Text = "Password";
+                txtPassword.ForeColor = Color.Silver;
+            }
         }
     }
 }
