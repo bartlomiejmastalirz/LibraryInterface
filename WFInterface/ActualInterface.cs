dc61061b2f6a183248f2ebd7acb385f9474bfdfb
ENRAEGE <jakub.m.2010@gmail.com>
2024-06-20 21:37:29 +0200

    Added memory control to the app

--- WFInterface/ActualInterface.cs
+++ WFInterface/ActualInterface.cs
@@ -15,6 +15,7 @@ namespace WFInterface
     {
         //DO NOT DELETE
         private User _currentUser;
+        private MyProfileForm _profileForm; // Store the instance of MyProfileForm
 
         //DO NOT DELETE
         public ActualInterface(User user)
@@ -25,11 +26,25 @@ namespace WFInterface
         }
 
         //DO NOT DELETE
-        private void btnlogout_Click(object sender, EventArgs e)
+        private void btnLogout_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            new Form1().Show();
-            MessageBox.Show("Succesfully logged out!");
+            Logout();
+        }
+
+        private void Logout()
+        {
+            Form1 loginForm = new Form1();
+            loginForm.Show();
+
+            if (_profileForm != null)
+            {
+                _profileForm.FormClosed -= MyProfileForm_FormClosed; // Unsubscribe event handler
+                _profileForm.Close();
+                _profileForm.Dispose();
+            }
+
+            this.Close(); // Close MainForm
+            this.Dispose();
         }
 
         private void TxtSearch_TextChanged(object sender, EventArgs e)
@@ -53,11 +68,22 @@ namespace WFInterface
             }
         }
 
+        //This button appears the My Profile form and hides this current one
         private void btnMyProfile_Click(object sender, EventArgs e)
         {
+            if (_profileForm == null || _profileForm.IsDisposed)
+            {
+                _profileForm = new MyProfileForm(_currentUser, this);
+                _profileForm.FormClosed += MyProfileForm_FormClosed; // Subscribe to the FormClosed event
+            }
+            PositionFormBehind(_profileForm, this); //This sets the position of the other form to this position
+            _profileForm.Show();
             this.Hide();
-            MyProfileForm profileForm = new MyProfileForm(_currentUser);
-            profileForm.Show();
+        }
+
+        private void MyProfileForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Show(); // Shows Actualinterface form again when MyProfileForm is closed
         }
 
         //DO NOT DELETE
@@ -66,16 +92,30 @@ namespace WFInterface
             lblUsername.Text = _currentUser.UserName;
         }
 
+        //Shows the Admin button to admin user
         private void ActualInterface_Load(object sender, EventArgs e)
         {
-            if (_currentUser.UserName != "AdminTest")
-            {
-                btnAdminPanel.Visible = false;
-            }
-            else
+            if (_currentUser.IsAdmin)
             {
                 btnAdminPanel.Visible = true;
             }
+            else
+            {
+                btnAdminPanel.Visible = false;
+            }
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PositionFormBehind(Form childForm, Form parentForm)
+        {
+            // Set the location of the child form to be the same as the parent form
+            childForm.StartPosition = FormStartPosition.Manual;
+            childForm.Location = parentForm.Location;
+        }
+
     }
 }
