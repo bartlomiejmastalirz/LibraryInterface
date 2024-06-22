a107b91d042d7ec7a17f485efdade06c10d31633
Student <Student@ST15-213>
2024-06-22 10:26:20 +0200

    tyes

--- WFInterface/ActualInterface.cs
+++ WFInterface/ActualInterface.cs
@@ -217,6 +217,16 @@ namespace WFInterface
         public string UserSurname { get; set; } = "";
         public bool IsAdmin { get; set; } = false;
         public List<int> BooksRented { get; set; } = new List<int>();
+
+        public void ParseBooksRented(string booksRented)
+        {
+            if (!string.IsNullOrEmpty(booksRented))
+            {
+                BooksRented = booksRented.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                                         .Select(int.Parse)
+                                         .ToList();
+            }
+        }
     }
 
     public class Book
