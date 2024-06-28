428260e0426115ff45312d6c77045b32ca80e036
Fabianm2003 <Fabi2003poczta@gmail.com>
2024-06-28 19:53:40 +0200

    Changes on the Admin Panel

--- WFInterface/Forms/AdminPanel.Designer.cs
+++ WFInterface/Forms/AdminPanel.Designer.cs
@@ -28,11 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnAddUser = new Button();
-            btnRemoveUser = new Button();
-            btnAddBook = new Button();
             btnBackToMenu = new Button();
-            btnRemoveBook = new Button();
             panel1 = new Panel();
             labelNameless = new Label();
             pictureBox1 = new PictureBox();
@@ -42,48 +38,27 @@
             panel5 = new Panel();
             panel4 = new Panel();
             panel3 = new Panel();
+            panel2 = new Panel();
+            panel8 = new Panel();
+            lblAuthorAdd = new Label();
+            lblAuthorRm = new Label();
+            lblYearAdd = new Label();
+            lblYearRm = new Label();
+            lblTitleAdd = new Label();
+            lblTitleRm = new Label();
+            btnAddBook = new Button();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            panel8.SuspendLayout();
             SuspendLayout();
-            // 
-            // btnAddUser
-            // 
-            btnAddUser.BackColor = Color.FromArgb(217, 189, 165);
-            btnAddUser.FlatAppearance.BorderSize = 0;
-            btnAddUser.FlatStyle = FlatStyle.Flat;
-            btnAddUser.Font = new Font("Calibri", 18F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            btnAddUser.Location = new Point(191, 133);
-            btnAddUser.Name = "btnAddUser";
-            btnAddUser.Size = new Size(249, 73);
-            btnAddUser.TabIndex = 0;
-            btnAddUser.Text = "Add user";
-            btnAddUser.UseVisualStyleBackColor = false;
-            // 
-            // btnRemoveUser
-            // 
-            btnRemoveUser.BackColor = Color.FromArgb(217, 189, 165);
-            btnRemoveUser.FlatAppearance.BorderSize = 0;
-            btnRemoveUser.FlatStyle = FlatStyle.Flat;
-            btnRemoveUser.Font = new Font("Calibri", 18F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            btnRemoveUser.Location = new Point(465, 133);
-            btnRemoveUser.Name = "btnRemoveUser";
-            btnRemoveUser.Size = new Size(262, 73);
-            btnRemoveUser.TabIndex = 1;
-            btnRemoveUser.Text = "Remove user";
-            btnRemoveUser.UseVisualStyleBackColor = false;
-            // 
-            // btnAddBook
-            // 
-            btnAddBook.BackColor = Color.FromArgb(217, 189, 165);
-            btnAddBook.FlatAppearance.BorderSize = 0;
-            btnAddBook.FlatStyle = FlatStyle.Flat;
-            btnAddBook.Font = new Font("Calibri", 18F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            btnAddBook.Location = new Point(191, 234);
-            btnAddBook.Name = "btnAddBook";
-            btnAddBook.Size = new Size(249, 73);
-            btnAddBook.TabIndex = 2;
-            btnAddBook.Text = "Add book";
-            btnAddBook.UseVisualStyleBackColor = false;
             // 
             // btnBackToMenu
             // 
@@ -98,19 +73,6 @@
             btnBackToMenu.Text = "Back to menu";
             btnBackToMenu.UseVisualStyleBackColor = false;
             btnBackToMenu.Click += btnBackToMenu_Click;
-            // 
-            // btnRemoveBook
-            // 
-            btnRemoveBook.BackColor = Color.FromArgb(217, 189, 165);
-            btnRemoveBook.FlatAppearance.BorderSize = 0;
-            btnRemoveBook.FlatStyle = FlatStyle.Flat;
-            btnRemoveBook.Font = new Font("Calibri", 18F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            btnRemoveBook.Location = new Point(465, 234);
-            btnRemoveBook.Name = "btnRemoveBook";
-            btnRemoveBook.Size = new Size(262, 73);
-            btnRemoveBook.TabIndex = 4;
-            btnRemoveBook.Text = "Remove book";
-            btnRemoveBook.UseVisualStyleBackColor = false;
             // 
             // panel1
             // 
@@ -149,7 +111,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(90, 621);
+            label1.Location = new Point(25, 686);
             label1.Name = "label1";
             label1.Size = new Size(161, 20);
             label1.TabIndex = 21;
@@ -158,7 +120,7 @@
             // panel7
             // 
             panel7.BackColor = Color.FromArgb(217, 189, 165);
-            panel7.Location = new Point(225, 645);
+            panel7.Location = new Point(160, 710);
             panel7.Margin = new Padding(3, 4, 3, 4);
             panel7.Name = "panel7";
             panel7.Size = new Size(32, 33);
@@ -167,7 +129,7 @@
             // panel6
             // 
             panel6.BackColor = Color.FromArgb(229, 203, 186);
-            panel6.Location = new Point(186, 645);
+            panel6.Location = new Point(121, 710);
             panel6.Margin = new Padding(3, 4, 3, 4);
             panel6.Name = "panel6";
             panel6.Size = new Size(32, 33);
@@ -176,7 +138,7 @@
             // panel5
             // 
             panel5.BackColor = Color.FromArgb(229, 222, 207);
-            panel5.Location = new Point(147, 645);
+            panel5.Location = new Point(82, 710);
             panel5.Margin = new Padding(3, 4, 3, 4);
             panel5.Name = "panel5";
             panel5.Size = new Size(32, 33);
@@ -185,7 +147,7 @@
             // panel4
             // 
             panel4.BackColor = Color.FromArgb(238, 231, 215);
-            panel4.Location = new Point(108, 645);
+            panel4.Location = new Point(43, 710);
             panel4.Margin = new Padding(3, 4, 3, 4);
             panel4.Name = "panel4";
             panel4.Size = new Size(32, 33);
@@ -194,11 +156,169 @@
             // panel3
             // 
             panel3.BackColor = Color.FromArgb(246, 238, 227);
-            panel3.Location = new Point(69, 645);
+            panel3.Location = new Point(4, 710);
             panel3.Margin = new Padding(3, 4, 3, 4);
             panel3.Name = "panel3";
             panel3.Size = new Size(32, 33);
             panel3.TabIndex = 16;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(229, 203, 186);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(btnAddBook);
+            panel2.Controls.Add(lblTitleAdd);
+            panel2.Controls.Add(lblYearAdd);
+            panel2.Controls.Add(lblAuthorAdd);
+            panel2.Location = new Point(101, 165);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(339, 233);
+            panel2.TabIndex = 22;
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.FromArgb(229, 203, 186);
+            panel8.Controls.Add(textBox4);
+            panel8.Controls.Add(textBox5);
+            panel8.Controls.Add(textBox6);
+            panel8.Controls.Add(button1);
+            panel8.Controls.Add(lblTitleRm);
+            panel8.Controls.Add(lblYearRm);
+            panel8.Controls.Add(lblAuthorRm);
+            panel8.Location = new Point(486, 165);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(339, 233);
+            panel8.TabIndex = 23;
+            // 
+            // lblAuthorAdd
+            // 
+            lblAuthorAdd.AutoSize = true;
+            lblAuthorAdd.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            lblAuthorAdd.Location = new Point(20, 22);
+            lblAuthorAdd.Name = "lblAuthorAdd";
+            lblAuthorAdd.Size = new Size(83, 28);
+            lblAuthorAdd.TabIndex = 0;
+            lblAuthorAdd.Text = "Author:";
+            // 
+            // lblAuthorRm
+            // 
+            lblAuthorRm.AutoSize = true;
+            lblAuthorRm.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            lblAuthorRm.Location = new Point(28, 22);
+            lblAuthorRm.Name = "lblAuthorRm";
+            lblAuthorRm.Size = new Size(83, 28);
+            lblAuthorRm.TabIndex = 1;
+            lblAuthorRm.Text = "Author:";
+            // 
+            // lblYearAdd
+            // 
+            lblYearAdd.AutoSize = true;
+            lblYearAdd.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            lblYearAdd.Location = new Point(20, 66);
+            lblYearAdd.Name = "lblYearAdd";
+            lblYearAdd.Size = new Size(57, 28);
+            lblYearAdd.TabIndex = 1;
+            lblYearAdd.Text = "Year:";
+            // 
+            // lblYearRm
+            // 
+            lblYearRm.AutoSize = true;
+            lblYearRm.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            lblYearRm.Location = new Point(28, 66);
+            lblYearRm.Name = "lblYearRm";
+            lblYearRm.Size = new Size(57, 28);
+            lblYearRm.TabIndex = 2;
+            lblYearRm.Text = "Year:";
+            // 
+            // lblTitleAdd
+            // 
+            lblTitleAdd.AutoSize = true;
+            lblTitleAdd.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            lblTitleAdd.Location = new Point(20, 109);
+            lblTitleAdd.Name = "lblTitleAdd";
+            lblTitleAdd.Size = new Size(58, 28);
+            lblTitleAdd.TabIndex = 3;
+            lblTitleAdd.Text = "Title:";
+            // 
+            // lblTitleRm
+            // 
+            lblTitleRm.AutoSize = true;
+            lblTitleRm.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            lblTitleRm.Location = new Point(28, 109);
+            lblTitleRm.Name = "lblTitleRm";
+            lblTitleRm.Size = new Size(58, 28);
+            lblTitleRm.TabIndex = 4;
+            lblTitleRm.Text = "Title:";
+            // 
+            // btnAddBook
+            // 
+            btnAddBook.BackColor = Color.FromArgb(217, 189, 165);
+            btnAddBook.FlatAppearance.BorderSize = 0;
+            btnAddBook.FlatStyle = FlatStyle.Flat;
+            btnAddBook.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            btnAddBook.Location = new Point(74, 162);
+            btnAddBook.Name = "btnAddBook";
+            btnAddBook.Size = new Size(202, 54);
+            btnAddBook.TabIndex = 4;
+            btnAddBook.Text = "Add book";
+            btnAddBook.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(217, 189, 165);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button1.Location = new Point(69, 162);
+            button1.Name = "button1";
+            button1.Size = new Size(202, 54);
+            button1.TabIndex = 5;
+            button1.Text = "Remove book";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(109, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(204, 27);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(84, 109);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(204, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(83, 69);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(204, 27);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(92, 109);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(204, 27);
+            textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(91, 66);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(204, 27);
+            textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(117, 22);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(204, 27);
+            textBox6.TabIndex = 10;
             // 
             // AdminPanel
             // 
@@ -206,6 +326,8 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(246, 238, 227);
             ClientSize = new Size(943, 748);
+            Controls.Add(panel8);
+            Controls.Add(panel2);
             Controls.Add(label1);
             Controls.Add(panel7);
             Controls.Add(panel6);
@@ -213,27 +335,22 @@
             Controls.Add(panel4);
             Controls.Add(panel3);
             Controls.Add(panel1);
-            Controls.Add(btnRemoveBook);
-            Controls.Add(btnAddBook);
-            Controls.Add(btnRemoveUser);
-            Controls.Add(btnAddUser);
             Name = "AdminPanel";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "AdminPanel";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel8.ResumeLayout(false);
+            panel8.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Button btnAddUser;
-        private Button btnRemoveUser;
-        private Button btnAddBook;
         private Button btnBackToMenu;
-        private Button btnRemoveBook;
         private Panel panel1;
         private Label labelNameless;
         private PictureBox pictureBox1;
@@ -243,5 +360,21 @@
         private Panel panel5;
         private Panel panel4;
         private Panel panel3;
+        private Panel panel2;
+        private Panel panel8;
+        private Button btnAddBook;
+        private Label lblTitleAdd;
+        private Label lblYearAdd;
+        private Label lblAuthorAdd;
+        private Button button1;
+        private Label lblTitleRm;
+        private Label lblYearRm;
+        private Label lblAuthorRm;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
     }
 }
